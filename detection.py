60098a2ae9cab465009fab5a1c063c37b2f1b84f
fatalfeel@hotmail.com <fatalfeel@hotmail.com>
2021-12-31 02:08:29 +0800

    faster label

--- detection.py
+++ detection.py
@@ -75,16 +75,18 @@ class Detection(nn.Module):
             labels[proposal_max_ious < 0.5] = 0
             fg_masks = (proposal_max_ious >= 0.5)
 
+            '''this section will get true class and put class number into labels'''
             #if len(fg_masks.nonzero()) > 0:
             #    labels[fg_masks] = gt_labels_batch[fg_masks.nonzero()[:, 0], proposal_assignments[fg_masks]]
             true_indices = torch.nonzero(fg_masks)                                              # get row col of ture fg_masks
             if len(true_indices) > 0:                                                           # make sure there is true index to process
                 morethan_p5_row     = true_indices[:, 0]                                        # row index
-                #proposal_iou_col   = proposal_assignments[fg_masks]                            # if fg_masks is ture then give column index
                 morethan_p5_col     = true_indices[:, 1]
-                proposal_iou_col    = proposal_assignments[morethan_p5_row, morethan_p5_col]    #same as proposal_assignments[fg_masks] but faster
-                gt_match_class      = gt_labels_batch[morethan_p5_row, proposal_iou_col]
-                labels[fg_masks]    = gt_match_class                                            #if fg_masks is ture then give gt_class number
+                # proposal_max_col  = proposal_assignments[fg_masks]                            # if fg_masks is ture then give column index
+                proposal_max_col    = proposal_assignments[morethan_p5_row, morethan_p5_col]    #same as proposal_assignments[fg_masks] but faster
+                gt_match_class      = gt_labels_batch[morethan_p5_row, proposal_max_col]
+                #labels[fg_masks]   = gt_match_class                                            #if fg_masks is ture then give gt_class number
+                labels[morethan_p5_row, morethan_p5_col] = gt_match_class                       # same as labels[fg_masks] but faster
 
             # select 128 x `batch_size` samples
             '''
